e1c6273986317c8abd304b720165a94fb17511d5
Daniel Håkansson <daniel.hakansson@yh.nackademin.se>
2022-04-19 15:11:57 +0200

    andra commit

---
+++ example.js
@@ -0,0 +1,23 @@
+
+/*
+let msg = 'hej hej';
+console.log(msg);
+*/
+
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+console.log("current time is " + (today));
+
+//update
+
+/*
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+*/
